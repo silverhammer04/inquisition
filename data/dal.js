d77ac1cc06d0c12bfbecd463209503a0877d90da
Maxwell Seifert <silverhammer04@yahoo.com>
2020-08-06 15:32:22 -0600

    Added POST to DAL and Service

--- data/dal.js
+++ data/dal.js
@@ -23,7 +23,38 @@ const readOrdos = () => {
     });
     return iou;
 };
+const readOrdoByID = (id) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, options, (err, client) => {
+            assert.equal(err, null);           
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+            collection.find({_id: new ObjectID(id)}).toArray((err, docs) => {
+                assert.equal(err, null);
+                resolve(docs[0]);
+                client.close();
+                })
+            });
+        });
+    return iou;
+}
 
+const createOrdos = (ordo) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, options, (err, client) => {
+            assert.equal(err, null);
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+            collection.insertOne(ordo, (err, result) => {
+                assert.equal(err, null);
+                resolve(result.ops[0]);
+                client.close();
+            })
+        });
+    });
+    return iou;
+}
 module.exports = {
+    createOrdos,
     readOrdos
 };
--- routes/api/ordos.js
+++ routes/api/ordos.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {
+    createOrdos,
     readOrdos
 } = require('../../data/dal')
 
 router.get('/', async function(req, res) {
     const data = await readOrdos();
+        res.send(data);
+});
+
+router.post('/', async function(req, res) {
+    const body = req.body;
+    const data = await createOrdos(body);
         res.send(data);
 });
 
